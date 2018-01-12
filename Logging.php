1e3eb53211846bbd2eac7360fe80483f3965ba71
bloodtear <xiaoyu1120520@126.com>
2018-01-12 17:22:09 +0800

    logging::w添加

--- Logging.php
+++ Logging.php
@@ -26,32 +26,69 @@ class Logging {
 
   // Database
   public static function d($module, $input){
-    self::write($module, $input,"D");
+    self::instance()->write($module, $input, "D");
   }
 
   // Portal
   public static function p($module, $input){
-    self::write($module, $input,"P");
+    self::instance()->write($module, $input, "P");
   }
 
   // Error
   public static function e($module, $input){
-    self::write($module, $input,"E");
+    self::instance()->write($module, $input, "E");
   }
 
   // Tpl
   public static function t($module, $input){
-    self::write($module, $input,"T");
+    self::instance()->write($module, $input, "T");
   }
 
   // Log
   public static function l($module, $input){
-    self::write($module, $input,"L");
+    self::instance()->write($module, $input, "L");
+  }
+
+  // websocket
+  public static function w($module, $input){
+    self::instance()->write($module, $input, "W", false);
   }
 
 
-  private static function write($module, $input, $level = "D"){
-    // 确定日志根目录
+  private function write($module, $input, $level = "D", $extra = true){
+    
+    $file_path = $this->file_path();
+    $now = $this->nowtime();
+
+    if ($extra) {
+      $host_ip = $this->remote_addr();
+      $host_port = $this->remote_port();
+      $username = $this->username();
+    }
+    // 整合输出内容
+    $output = str_pad("<$now>", 30, ' ', STR_PAD_RIGHT);
+
+    if ($extra) {
+      $output .= str_pad("<$host_ip>", 20, ' ', STR_PAD_RIGHT);
+      $output .= str_pad("<$host_port>", 8, ' ', STR_PAD_RIGHT);
+      $output .= str_pad("$username", 20, ' ', STR_PAD_RIGHT);
+    }
+
+    $output .= str_pad("$level/$module", 20, ' ', STR_PAD_RIGHT);    
+    if (!is_string($input)) {
+      $input = json_encode($input);
+    }
+    $output .= $input . "\n";
+
+    // 打开&&写入文件
+    touch($file_path);
+    $file = fopen($file_path, "a");
+    fwrite($file, $output);
+    fclose($file);
+
+  }
+
+  private function file_path() { // 确定日志根目录
     $path = self::$path;
     if(!file_exists($path)){
       mkdir($path, 0777);
@@ -66,57 +103,28 @@ class Logging {
 
     // 日志文件
     $file = "Logging-" . date("Y-m-d") . ".txt";
-    $file_path = $path_year . $file;
+    return $path_year . $file;
+  } 
 
-
-    // 确定log时间
+  private function nowtime() { // 确定log时间
     list($micro, $stamp) = explode(' ', microtime());
     $micro = str_pad(round($micro, 3) * 1000, 3, "0", STR_PAD_RIGHT);   // 取三位并且后缀补零
-    $now = date('Y-m-d H:i:s', $stamp) . ":$micro"; 
-    //var_dump($now);
+    return date('Y-m-d H:i:s', $stamp) . ":$micro"; 
+  } 
 
-    // 确定log的ip
-    $host_ip = $_SERVER['REMOTE_ADDR'];
-    //echo $host_ip;
-
-    // 确定log的port
-    $host_port = $_SERVER['REMOTE_PORT'];
-    //echo $host_port;
-
-    $username = 'NOT LOGIN';
-    // 确定当前地user
-    $username = !(empty($_SESSION['username'])) ? $_SESSION['username'] : "NOT LOGIN";
-
-    // 整合输出内容
-    $output = str_pad("<$now>", 30, ' ', STR_PAD_RIGHT);
-    $output .= str_pad("<$host_ip>", 20, ' ', STR_PAD_RIGHT);
-    $output .= str_pad("<$host_port>", 8, ' ', STR_PAD_RIGHT);
-    $output .= str_pad("$username", 20, ' ', STR_PAD_RIGHT);
-
-    $output .= str_pad("$level/$module", 20, ' ', STR_PAD_RIGHT);    
-    if (!is_string($input)) {
-      $input = json_encode($input);
-    }
-    $output .= $input . "\n";
-
-    // 打开&&写入文件
-    $file = fopen($file_path, "a");
-    fwrite($file, $output);
-    fclose($file);
-    touch($file_path);
-
+  private function remote_addr() { 
+    return $_SERVER['REMOTE_ADDR'];
   }
 
+  private function remote_port() { 
+    return $_SERVER['REMOTE_PORT'];
+  }
 
-
-
-
-
-
-
-
-
-
+  private function username() {
+    $username = 'NOT LOGIN';
+    // 确定当前地user
+    return !(empty($_SESSION['username'])) ? $_SESSION['username'] : "NOT LOGIN";
+  }
 
 
 }
