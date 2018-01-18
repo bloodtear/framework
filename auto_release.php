2ed74a732ccb2e557840ad49847775f74c23b9b3
bloodtear <xiaoyu1120520@126.com>
2018-01-18 17:45:56 +0800

    修改auto_release，已经验证可使用

--- auto_release.php
+++ auto_release.php
@@ -6,18 +6,21 @@ Logging::l("RELEASE", "release start.");
 chdir(APP_PATH);
 Logging::l("RELEASE", "now pwd is " . getcwd());
 
-$hook = get_request("hook");
 
-$secret = $hook["config"]["secret"];
+$req = get_request("payload");
+$req = json_decode($req);
+$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+$s = 'aabbcc123';
+$hash = "sha1=" . hash_hmac("sha1", file_get_contents("php://input"), $s);
 
-if ($secret !== 'aabbcc123') {
+if ($signature !== $hash) {
   Logging::e("RELEASE", "secret is fault.");
   return false;
 }
 
-$test = "git pull";
-$out = shell_exec($test);
-Logging::l("RELEASE", "out is " . $out);
+$shell = "git pull";
+$out = shell_exec($shell);
+Logging::l("RELEASE", $out);
 
 
 
